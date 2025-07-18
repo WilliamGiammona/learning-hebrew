ac39fee46662f0ead660997572a2871db1966cec
William Giammona <wgiammona@gmail.com>
2025-07-18 19:13:46 +0300

    initial commit

---
+++ app/verbs/nifal/page.tsx
@@ -0,0 +1,12 @@
+// app/verbs/nifal/page.tsx
+export default function VerbPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-4xl font-bold text-blue-800 mb-4 text-center">
+          Page
+        </h1>
+      </div>
+    </div>
+  );
+}
---
+++ app/verbs/paal/page.tsx
@@ -0,0 +1,13 @@
+// app/verbs/paal/page.tsx
+
+export default function VerbPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-4xl font-bold text-blue-800 mb-4 text-center">
+          Page
+        </h1>
+      </div>
+    </div>
+  );
+}
