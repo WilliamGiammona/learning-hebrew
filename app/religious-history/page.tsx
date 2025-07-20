a18691db2b9c47a9d424e822385006d2502449ab
William Giammona <wgiammona@gmail.com>
2025-07-20 12:31:11 +0300

    laid out sections

--- app/religious-history/page.tsx
+++ app/religious-history/page.tsx
@@ -1,7 +1,73 @@
 //app/religious-history/page.tsx
 
-const page = () => {
-  return <div>page</div>;
+const JewishHistory = () => {
+  return (
+    <>
+      <h1>
+        Brief Overview of Biblical and Religious History of Jewish People and
+        Judaism
+      </h1>
+      <section id="#Abraham">
+        <h1>Abraham</h1>
+        <p>
+          Abraham was born in Ur (modern day Iraq) during the very beginning of
+          the Babylonian empire. He was commanded by God to settle in the land
+          of Israel and is considered the first Jew to live in Israel and it was
+          to Abraham who God promised that the land would be given to the Jews
+        </p>
+
+        <p>
+          Abraham had _ sons, one of them was named Isaac from whom all Jews are
+          descended, the other is Ishmail, from whom all Arabs are descended.
+        </p>
+      </section>
+      <section id="#12-Tribes">
+        <h1> 12 Tribes of Israel</h1>
+        <p>
+          Isaac had a son Jacob. Jacob had 12 sons, these are the fathers of the
+          12 tribes of Israel that make up the Jewish people. They are the: 1)
+          2) 3) 4) 5) 6) 7) 8) 9) 10) 11) 12)
+        </p>
+      </section>
+      <section id="#Egypt">
+        <h1>Egypt</h1>
+        <p>
+          Around this time, the New Kingdom of Egypt was at its most powerful,
+          and conquered the land of Israel. They enslaved the Jews. Moses Levi
+          lead the Jews out of Israel and his brother Aaron was a Cohen
+        </p>
+      </section>
+      <section id="#10-Commandments"></section>
+      <section id="#Into Israel"></section>
+      <section id="#12-Tribes-Divided"></section>
+      <section id="#Saul"></section>
+      <section id="#Civil-War"></section>
+      <section id="#King-David"></section>
+      <section id="#Solomon"></section>
+      <section id="#Two-States-Samaritans"></section>
+      <section id="#Assyrian-Invasion"></section>
+      <section id="#Babylonian Captivity"></section>
+      <section id="3Return to Israel"></section>
+      <section id="#Macedon"></section>
+      <section id="#Hasmonian-Kingdom"></section>
+      <section id="#Pompey"></section>
+      <section id="#Herod"></section>
+      <section id="#Jesus-3-sects"></section>
+      <section id="#First-Revolt"></section>
+      <section id="#Jehuda-HaNasi"></section>
+      <section id="#Karite-Jews"></section>
+      <section id="#Two-Talmuds"></section>
+      <section id="#Sephardic-Jews"></section>
+      <section id="#Maimodines"></section>
+      <section id="#Kabala"></section>
+      <section id="#Shulchan-Aruch"></section>
+      <section id="#Hasidic"></section>
+      <section id="#Litvish"></section>
+      <section id="#Rabbi-Kook"></section>
+      <section id="#Rabbi-Kahane"></section>
+      <section id="#Ovidia-Yosef"></section>
+    </>
+  );
 };
 
-export default page;
+export default JewishHistory;
