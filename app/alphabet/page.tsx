13df31fbebf46f9f563edf08c1196d4946c04e15
William Giammona <wgiammona@gmail.com>
2025-07-22 07:56:49 -0700

    beginning the Alphabet

--- app/alphabet/page.tsx
+++ app/alphabet/page.tsx
@@ -3,10 +3,17 @@
 const Alphabet = () => {
   return (
     <>
-      <section id="#Formal-Letters"></section>
+      <section id="#Formal-Letters">
+        <h1>The Alphabet</h1>
+        <p>
+          Hebrew is read from right to left and consists of 22 letters. Each
+          letter has a name and a numerical value.
+        </p>
+      </section>
       <section id="#Vowels"></section>
       <section id="#Cursive"></section>
-      <section id="Ancient-Hebrew"></section>
+      <section id="#Numbers"></section>
+      <section id="#Ancient-Hebrew"></section>
     </>
   );
 };
